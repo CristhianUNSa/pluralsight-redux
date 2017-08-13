9c7ac5304db02a220a61ad71e367550815b521a4
Cristhian Valencia <valenciacristhian47@gmail.com>
2017-08-13 20:01:46 -0300

    Código inicial con routing

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class AboutPage extends React.Component {
+  render() {
+    return <div>About page</div>;
+  }
+}
+
+export default AboutPage;
